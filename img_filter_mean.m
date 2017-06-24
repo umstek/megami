96ce85c6257d7abf2d75e1bc110f797bd23fd784
Wickramaranga Abeygunawardhana <umstek@live.com>
2017-06-24 13:08:49 +0530

    Simple mean and median filters

---
+++ img_filter_mean.m
@@ -0,0 +1,18 @@
+%% Apply mean filter
+
+I = im2double(imread('fishingboat.tif'));
+
+bs = 3;
+overflow = ceil(bs/2);
+
+paddedI = zeros(size(I) + bs-1);
+paddedI(overflow:end-overflow+1, overflow:end-overflow+1) = I;
+I2 = zeros(size(I));
+
+for i = 1:size(I, 1)
+    for j = 1:size(I, 2)
+        I2(i, j) = mean(reshape(paddedI(i+1-1:i+bs-1, j+1-1:j+bs-1)', bs*bs, 1));
+    end
+end
+
+imshow(I2);
